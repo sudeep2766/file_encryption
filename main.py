ea7c0141f0fe43dd4236d754e2fffbef78b0688a
sudeep2766 <158793007+sudeep2766@users.noreply.github.com>
2024-10-24 17:31:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,17 +13,24 @@ for image in image_list:
     else:
         to_encrypt.append(image)
 
-print(to_encrypt)
 key = fernet.Fernet.generate_key()
 
-with open("thekey.key", 'wb') as thekey:
-    thekey.write(key)
+if os.path.exists('thekey.key'):
+    opt = input("Password file already exists. Are you sure on encrypting? (Y/n)")
+    if opt.lower() == 'y':
+        opt2 = input("Are you sure about that? If already encrypted the password will be overwritten and cannot be recovered (Y/n)")
+        if opt2.lower() == 'y':
+            with open ('thekey.key', 'wb') as thekey1:
+                thekey1.write(key)
 
-for file in to_encrypt:
-    with open(file, 'rb')as encrypting:
-        contents = encrypting.read()
-    
-    encrypted = fernet.Fernet(key).encrypt(contents)
+            for file in to_encrypt:
+                with open(file, 'rb')as encrypting:
+                    contents = encrypting.read()
+        
+                encrypted = fernet.Fernet(key).encrypt(contents)
 
-    with open(file, 'wb') as just_encrypting:
-        just_encrypting.write(encrypted)
+                with open(file, 'wb') as just_encrypting:
+                    just_encrypting.write(encrypted)
+
+print("Encryption successful!")
+print(to_encrypt)
